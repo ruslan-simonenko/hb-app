accc4b9366eef76e494628746d3912d37fa8f003
Igor Leshchenko <ileshchenko@cogniance.com>
2013-07-02 23:38:12 +0300

    Folder structure improvements

---
+++ client/javascript/app.js
@@ -0,0 +1,13 @@
+var app = {
+
+	models: {}, views: {}, settings: {}, storage: { models: [], views: [] },
+
+	init: function() {
+		console.log('here we are');
+	} 
+};
+
+
+$(document).ready(function() {
+	app.init();
+});
